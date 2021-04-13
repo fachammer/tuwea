bc880bcfb78be90016ede48c9eaf99cc689905cc
Fabian Achammer <fabian@achammer.dev>
2021-04-13 21:33:19 +0200

    display student exercise assignment status

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,8 @@ kotlin {
                 implementation(kotlin("stdlib-js"))
                 implementation("org.jetbrains:kotlin-react:17.0.1-pre.148-kotlin-1.4.21")
                 implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.148-kotlin-1.4.21")
+                implementation("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.21")
+                implementation(npm("styled-components", "~5.2.1"))
                 implementation(npm("react", "17.0.1"))
                 implementation(npm("react-dom", "17.0.1"))
             }
--- src/jsMain/kotlin/dev/achammer/tuwea/browser/Main.kt
+++ src/jsMain/kotlin/dev/achammer/tuwea/browser/Main.kt
@@ -2,8 +2,9 @@ package dev.achammer.tuwea.browser
 
 import dev.achammer.tuwea.core.*
 import kotlinx.browser.document
+import kotlinx.css.Color
+import kotlinx.css.backgroundColor
 import kotlinx.html.InputType
-import kotlinx.html.id
 import kotlinx.html.js.onChangeFunction
 import kotlinx.html.js.onClickFunction
 import org.w3c.files.Blob
@@ -13,6 +14,9 @@ import react.child
 import react.dom.*
 import react.functionalComponent
 import react.useState
+import styled.css
+import styled.styledTd
+import styled.styledTr
 
 val app = functionalComponent<RProps> {
     val (parseResult, setParseResult) = useState<ParseResult?>(null)
@@ -36,12 +40,18 @@ val app = functionalComponent<RProps> {
             }
             tbody {
                 parseResult.studentCheckmarksEntries.forEach { studentCheckmarksEntry ->
-                    tr {
+                    styledTr {
+                        css {
+                            if (exerciseStudentAssignment != null &&
+                                exerciseStudentAssignment.any { it.second == studentCheckmarksEntry }
+                            ) {
+                                backgroundColor = Color.lightGray
+                            }
+                        }
                         td { +"${studentCheckmarksEntry.firstName} ${studentCheckmarksEntry.lastName}" }
                         td {
                             input(type = InputType.checkBox) {
                                 attrs {
-                                    id = studentCheckmarksEntry.idNumber
                                     checked = isStudentPresent(studentCheckmarksEntry)
                                     onChangeFunction = { event: dynamic ->
                                         if (isStudentPresent(studentCheckmarksEntry)) {
@@ -54,7 +64,15 @@ val app = functionalComponent<RProps> {
                             }
                         }
                         parseResult.exercises.forEach { exercise ->
-                            td {
+                            styledTd {
+                                css {
+                                    if (exerciseStudentAssignment != null
+                                        && exerciseStudentAssignment.any { it.first == exercise && it.second == studentCheckmarksEntry }
+                                    ) {
+                                        backgroundColor = Color.red
+                                    }
+                                }
+
                                 if (studentCheckmarksEntry.checkmarks.contains(exercise)) {
                                     +"X"
                                 } else {
@@ -101,6 +119,8 @@ val app = functionalComponent<RProps> {
                 reader.onload = { it: dynamic ->
                     val content = it.target.result as String
                     setParseResult(parseCsvContent(tuwelParseConfiguration, content))
+                    setExerciseStudentAssignment(null)
+                    setPresentSet(emptySet())
                 }
                 reader.readAsText(file)
             }
